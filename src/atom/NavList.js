4e02887eba7f7e5f5c0c2695dfe856c44cf2d6f7
kdn0325 <sexy-ehdsud@nate.com>
2022-02-18 01:46:52 +0900

    movie

--- src/atom/NavList.js
+++
@@ -1,3 +0,0 @@
-const Group ={ "High Rating": "minimum_rating=8", "Romance": "genre=romance", "Music": "genre=music", "Animation": "genre=animation" }
-
-export {Group};
--- src/route/Detail.js
+++ src/route/Detail.js
@@ -1,37 +1,16 @@
-
 import { useEffect } from "react";
-import {useParams} from "react-router-dom"
-function Detail(){
-    const {id} = useParams()
-    const getMovie = async()=> {
-        const json = await
-        (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`))
-        .json();
-        console.log(json)
-    };
-    
-    useEffect(()=>{
-        getMovie();
-        
-    },[])
-    return(
-        <div>
-           {loading ?( <h1>Loading...</h1>) : ( 
-           <div>
-           {movies.map((movie) => (
-             <Movie
-                key={movie.id}
-                id={movie.id}
-                coverImg={movie.medium_cover_image}
-                title={movie.title}
-                summary={movie.summary}
-                genres={movie.genres}
-                rating={movie.rating}
-             />
-           ))}
-           </div>
-        )}
-        </div>
-    )
+import { useParams } from "react-router-dom";
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return <h1>Detail</h1>;
 }
 export default Detail;
